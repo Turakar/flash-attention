62ef4552b26c06ac3fb2ecd9e8968829bd9b7712
Tilman Hoffbauer <turakar23@gmail.com>
2025-02-14 13:57:16 +0100

    Rename hopper flash_attn to flash_attn3

--- hopper/setup.py
+++ hopper/setup.py
@@ -33,7 +33,7 @@ with open("../README.md", "r", encoding="utf-8") as fh:
 # ninja build does not work unless include_dirs are abs path
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
-PACKAGE_NAME = "flash_attn"
+PACKAGE_NAME = "flash_attn3"
 
 BASE_WHEEL_URL = "https://github.com/Dao-AILab/flash-attention/releases/download/{tag_name}/{wheel_name}"
 
